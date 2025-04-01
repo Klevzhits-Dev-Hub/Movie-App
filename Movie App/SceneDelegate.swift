255b2eeec4665a8c393d9d6c82103972c8333588
Igor Klevzhits <klevzhits94@icloud.com>
2025-04-01 11:30:30 +0300

    MOV-11: Add Fonts

--- Movie App/SceneDelegate.swift
+++ Movie App/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let searchViewController = ViewController()
         let recentWatchViewController = ViewController()
-        let homeViewController = ViewController() // Что это за экран??)
+        let homeViewController = ViewController()
         let wishlistViewController = ViewController()
         let settingsViewController = ViewController()
         
