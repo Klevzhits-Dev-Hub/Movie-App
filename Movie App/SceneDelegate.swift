eb6bcf3539ced37c80ce984fd94f1105655a9fd1
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-04-01 20:53:02 -0300

    MOV-3: merge with develop

--- Movie App/SceneDelegate.swift
+++ Movie App/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let searchViewController = ViewController()
         let recentWatchViewController = ViewController()
-        let homeViewController = ViewController()
+        let homeViewController = HomeViewController()
         let wishlistViewController = WishlistViewController()
         let settingsViewController = ViewController()
         
